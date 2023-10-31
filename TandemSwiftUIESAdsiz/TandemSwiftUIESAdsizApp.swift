e9254e1f375fc8f7b0ddc2306b3adc310197bd59
E. S. Adsiz <enessaitadsiz@gmail.com>
2023-10-31 22:15:06 +0100

    Initial Commit

---
+++ TandemSwiftUIESAdsiz/TandemSwiftUIESAdsizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TandemSwiftUIESAdsizApp.swift
+//  TandemSwiftUIESAdsiz
+//
+//  Created by Enes Sait Adsiz on 31.10.23.
+//
+
+import SwiftUI
+
+@main
+struct TandemSwiftUIESAdsizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
